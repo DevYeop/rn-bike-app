5e34504b50c33bca957577d845126de97cf51090
Gbike <gbike@Gbikes-MacBook-Pro.local>
2020-10-13 15:21:43 +0900

    googleLoginButton 구현 완료

--- src/ui/login/GoogleLoginButton.js
+++ src/ui/login/GoogleLoginButton.js
@@ -4,40 +4,25 @@ import {
     GoogleSigninButton,
     statusCodes,
 } from '@react-native-community/google-signin';
-
-
-
-_signIn = async () => {
-    try {
-        await GoogleSignin.hasPlayServices();
-        const userInfo = await GoogleSignin.signIn();
-
-        // this.props.saveUserInfoGoogle(userInfo)
-
-        // this.getRouteItems()
-
-        // this.getContactList()
-
-        // this.setUserInfoFireStore()
-
-        this.props.navigation.navigate('BottomTapNavigator')
-    } catch (error) {
-        if (error.code === statusCodes.SIGN_IN_CANCELLED) {
-            console.log('SIGN_IN_CANCELLED')
-        } else if (error.code === statusCodes.IN_PROGRESS) {
-            console.log('IN_PROGRESS')
-        } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
-            console.log('PLAY_SERVICES_NOT_AVAILABLE')
-        } else {
-            console.error(error);
-        }
-    }
-};
-
+ 
 export const GoogleLoginButton = (initSetting) => {
-
-    const setInitSetting = () => {
-        initSetting
+ 
+    const signIn = async () => {
+        try {
+            await GoogleSignin.hasPlayServices();
+            const userInfo = await GoogleSignin.signIn();
+            initSetting.initSetting(userInfo, 'google')
+        } catch (error) {
+            if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+                console.log('SIGN_IN_CANCELLED')
+            } else if (error.code === statusCodes.IN_PROGRESS) {
+                console.log('IN_PROGRESS')
+            } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+                console.log('PLAY_SERVICES_NOT_AVAILABLE')
+            } else {
+                console.error(error);
+            }
+        }
     }
 
     return (
@@ -45,7 +30,7 @@ export const GoogleLoginButton = (initSetting) => {
             style={{ width: 250, height: 50, marginBottom: 25, }}
             size={GoogleSigninButton.Size.Wide}
             color={GoogleSigninButton.Color.Dark}
-            onPress={setInitSetting()} />
+            onPress={()=> signIn()} />
     )
 }
 
--- src/ui/login/SettingTab.js
+++ src/ui/login/SettingTab.js
@@ -1,31 +1,31 @@
 import React from 'react';
 import { Text, StyleSheet, View, StatusBar, SafeAreaView, Button, Image, TouchableOpacity } from 'react-native'
 import { connect } from 'react-redux'
-import { 
+import {
   saveUserInfoGoogle,
+  saveUserInfoKakao,
   setPreRouteItems,
   setContactItems,
- } from '../../actions/Actions'
+} from '../../actions/Actions'
 import { bindActionCreators } from 'redux'
 
 import {
   GoogleSignin,
-  GoogleSigninButton,
-  statusCodes,
 } from '@react-native-community/google-signin'
 
 import KakaoLoginButton from './KakaLoginButton'
 
 import firestore from '@react-native-firebase/firestore'
-import {loadRouteItem, loadContactList} from '../../query/accessFireStore'
+import { loadRouteItem, loadContactList } from '../../query/accessFireStore'
 import GoogleLoginButton from './GoogleLoginButton';
- 
+
 class SettingTap extends React.Component {
 
   constructor(props) {
     super(props)
   }
 
+  // ???
   componentDidMount() {
     GoogleSignin.configure({
       webClientId: '1038042430405-e2nl3grsb7aqt804d1tni51vkd7aaka1.apps.googleusercontent.com',
@@ -34,120 +34,57 @@ class SettingTap extends React.Component {
       forceConsentPrompt: true,
     })
   }
-  
-  async getContactList() { 
 
+  async getContactList() {
     const contactList = await loadContactList(this.props.userInfo.id)
-
     this.props.setContactItems(contactList)
-}
-  
-  async getRouteItems() {
-  
-    const routeItems = await loadRouteItem(this.props.userInfo.id)
-     
-    this.props.setPreRouteItems(routeItems)
-}
+  }
 
-  /**
-   * 유저의 fireStore 루트 Collection에 유저정보를 document로 저장한다.
-   * 나중에 친구 추가,검색 및 채팅 초대 등을 위해 유저정보를 참조하기 위해 쓰인다.
-   */
+  async getRouteItems() {
+    const routeItems = await loadRouteItem(this.props.userInfo.id)
+    this.props.setPreRouteItems(routeItems)
+  }
+
   setUserInfoFireStore = () => {
- 
+
     const userIndex = this.props.userInfo.id
-    const collectionName = 'user'+userIndex
- 
     const itemsRef = firestore().collection('userInfo');
- 
+
     itemsRef.doc(userIndex).set(
-        { 
-          id : this.props.userInfo.id,
-          email : this.props.userInfo.email,
-          nickname : this.props.userInfo.nickname,
-          profile_image_url : this.props.userInfo.profile_image_url
-        }
+      {
+        id: this.props.userInfo.id,
+        email: this.props.userInfo.email,
+        nickname: this.props.userInfo.nickname,
+        profile_image_url: this.props.userInfo.profile_image_url
+      }
     )
   }
- 
-  
-  _signIn = async () => {
-    try {
-      await GoogleSignin.hasPlayServices();
-      const userInfo = await GoogleSignin.signIn();
 
+  initSetting = (userInfo, from) => {
+    if (from == 'google') {
       this.props.saveUserInfoGoogle(userInfo)
- 
-      this.getRouteItems()
- 
-      this.getContactList()
-
-      this.setUserInfoFireStore()
-
-      this.props.navigation.navigate('BottomTapNavigator')
-    } catch (error) {
-      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
-        console.log('SIGN_IN_CANCELLED')
-      } else if (error.code === statusCodes.IN_PROGRESS) {
-        console.log('IN_PROGRESS')
-      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
-        console.log('PLAY_SERVICES_NOT_AVAILABLE')
-      } else {
-        console.error(error);
-      }
+    } else if (from == 'kakao') {
+      this.props.saveUserInfoKakao(userInfo)
     }
-  }
-
-  signOut = async () => {
-    try {
-      await GoogleSignin.revokeAccess();
-      await GoogleSignin.signOut();
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  initSetting() {
-
-    console.log('testtest')
-
-    // if (true) {
-    //   this.props.saveUserInfoGoogle(userInfo)
-    // } else {
-
-    // }
-
     this.getRouteItems()
-
     this.getContactList()
-
     this.setUserInfoFireStore()
-
+    this.props.navigation.navigate('BottomTapNavigator')
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.container}>
- 
-          <Image
-            style={{alignItems:'flex-start', height:500}}
-            source={require('../../res/brung.gif')}
-            resizeMethod='resize' />
 
-          <Text style={{fontSize:32}}> rn - bike - app  </Text>
-          
-          <GoogleLoginButton initSetting={this.initSetting} />
+        <Image
+          style={{ alignItems: 'flex-start', height: 500 }}
+          source={require('../../res/brung.gif')}
+          resizeMethod='resize' />
 
-          {/* <GoogleSigninButton
-            style={{ width: 250, height: 50, marginBottom : 25, }}
-            size={GoogleSigninButton.Size.Wide}
-            color={GoogleSigninButton.Color.Dark}
-            onPress={this._signIn}/> */}
+        <GoogleLoginButton initSetting={this.initSetting} />
 
-          <KakaoLoginButton navigation={this.props.navigation} />
+        <KakaoLoginButton initSetting={this.initSetting} />
 
-        </View>
       </View>
     )
   }
@@ -161,6 +98,7 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = dispatch => (
   bindActionCreators({
     saveUserInfoGoogle,
+    saveUserInfoKakao,
     setPreRouteItems,
     setContactItems,
   }, dispatch)
